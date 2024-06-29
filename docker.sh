33e1820dc88c33fa895416f1dd45bd3f3c709122
wlin33 <wlin33@ur.rochester.edu>
2024-06-29 09:16:33 -0400

    fix docker run

--- docker.sh
+++ docker.sh
@@ -2,6 +2,6 @@ docker run --gpus all \
            --name rtgs \
            -d -it \
            --network=host \
-           --volume /home/lwk/ur_research/FoV-3DGS/:/workspace \
+           --volume $PATH_TO_REPO/FoV-3DGS/:/workspace \
            pytorch/pytorch:2.3.0-cuda11.8-cudnn8-devel
 
